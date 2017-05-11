1ad4ba3865cffb77b85deaf2e454de31300cbaa8
RoxieZ <roxiezhao.rz@gmail.com>
2017-05-11 11:48:32 -0700

    port change

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express    = require('express'),      
 	  app        = express();
 
-app.use(express.static('public'));
-app.listen(3000);
+app.use(express.static(__dirname + '/public'));
+var port = process.env.PORT || 3000;
+app.listen(port);
