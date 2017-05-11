1aa0d36fbad144bf3e76d785f2155964643e354e
RoxieZ <roxiezhao.rz@gmail.com>
2017-05-11 11:57:51 -0700

    res.set added

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express    = require('express'),      
 	  app        = express();
 
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+
+    res.render('index');
+});
 var port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(port,function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
