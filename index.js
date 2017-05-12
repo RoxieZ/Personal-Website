36b458d55a4b0f93483d2039f79cc28012565b0d
RoxieZ <roxiezhao.rz@gmail.com>
2017-05-11 21:05:11 -0700

    test

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express    = require('express'),      
 	  app        = express();
 
+
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res) {
 
